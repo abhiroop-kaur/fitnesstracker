988b74134817ae9ea36df48c3c0f2f6c6601e497
Abhiroop kaur <125322699+abhiroop-kaur@users.noreply.github.com>
2024-12-06 06:15:14 -0700

    app.js

---
+++ 481fittracker/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import './App.css'; // Make sure to import your CSS file
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my React App!</h1>
+    </div>
+  );
+}
+
+export default App;
